5d75235ab009463dd3759798dc5100a06d66c2df
Tomas Jelinek <tomgolf.jelinek@gmail.com>
2024-12-21 13:52:16 +0100

    add selfie to metrics

--- loss.py
+++ loss.py
@@ -23,6 +23,7 @@ def calculate_loss(sdf_file: str, config_data: dict) -> float:
 
         real_sa_score = sascorer.calculateScore(mol)
         real_qed_score = QED.qed(mol)
+        selfie = mol.GetProp('SELFIE') 
 
         sa_score = config_data['sa_weight'] * (1 - real_sa_score/10)
         qed_score = config_data['qed_weight'] * real_qed_score
@@ -30,7 +31,7 @@ def calculate_loss(sdf_file: str, config_data: dict) -> float:
             get_docking_score(sdf_file)*-1
 
         loss_value = sa_score + qed_score + docking_score
-        return loss_value, {"SA": real_sa_score, "QED": real_qed_score, "Docking score": docking_score, "Adjusted SA": sa_score, "Adjusted QED": qed_score}
+        return loss_value, {"SA": real_sa_score, "QED": real_qed_score, "Docking score": docking_score, "Adjusted SA": sa_score, "Adjusted QED": qed_score, "SELFIE": selfie}
     except Exception as e:
         logging.error(f"Error during loss calculation of {sdf_file}: {e}")
-        return float('-inf'), {"SA": float('-inf'), "QED": float('-inf'), "Docking score": float('-inf'), "Adjusted SA": float('-inf'), "Adjusted QED": float('-inf')}
+        return float('-inf'), {"SA": float('-inf'), "QED": float('-inf'), "Docking score": float('-inf'), "Adjusted SA": float('-inf'), "Adjusted QED": float('-inf'), "SELFIE": ""}
